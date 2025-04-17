10051d130616f353724580597ece544baf4ebbb8
rinz <roberts.denisovs@gmail.com>
2025-04-17 19:14:36 +0300

    llvm intrinsics fix

--- build.zig
+++ build.zig
@@ -5,8 +5,8 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("llvm", .{
-        .root_source_file = b.path("src/llvm/llvm.zig"),
+    const llvm_module = b.addModule("rllvm", .{
+        .root_source_file = b.path("src/rllvm.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -16,4 +16,12 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    const tests = b.addTest(.{
+        .root_module = llvm_module,
+        .optimize = optimize,
+    });
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&b.addRunArtifact(tests).step);
 }
