f865b2601b77698e65cfab54451f4acfc263972a
rinz <roberts.denisovs@gmail.com>
2025-04-17 17:54:33 +0300

    gitignore

--- build.zig
+++ build.zig
@@ -5,14 +5,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("llvm", .{
-        .root_source_file = b.path("llvm/llvm.zig"),
+    _ = b.addModule("rrlvm", .{
+        .root_source_file = b.path("src/rrlvm.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     _ = b.addModule("clang", .{
-        .root_source_file = b.path("llvm/clang.zig"),
+        .root_source_file = b.path("src/llvm/clang.zig"),
         .target = target,
         .optimize = optimize,
     });
