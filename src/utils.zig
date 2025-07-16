908de8928b8cecea3e9157e7795ed463201764e0
oxrinz <roberts.denisovs@gmail.com>
2025-07-17 01:48:08 +0300

    bad prints implemented fix later

--- src/utils.zig
+++ src/utils.zig
@@ -24,14 +24,39 @@ const target_machine = @import("raw_llvm/target_machine.zig");
 const transform = @import("raw_llvm/transform.zig");
 const types = @import("raw_llvm/types.zig");
 
+var printf_func: types.LLVMValueRef = null;
+
 pub fn printConstString(module: types.LLVMModuleRef, builder: types.LLVMBuilderRef, str: [*:0]const u8) !void {
     const i8_ptr_ty = core.LLVMPointerType(core.LLVMInt8Type(), 0);
     var param_types = [_]types.LLVMTypeRef{ i8_ptr_ty, i8_ptr_ty };
     const printf_ty = core.LLVMFunctionType(core.LLVMInt32Type(), param_types[0..], 2, 1);
-    const printf_func = core.LLVMAddFunction(module, "printf", printf_ty);
+    if (printf_func == null) printf_func = core.LLVMAddFunction(module, "printf", printf_ty);
 
     const fmt_str = core.LLVMBuildGlobalStringPtr(builder, "%s", "fmt");
     const str_val = core.LLVMBuildGlobalStringPtr(builder, str, "str");
     var args = [_]types.LLVMValueRef{ fmt_str, str_val };
+    _ = core.LLVMBuildCall2(builder, printf_ty, printf_func, args[0..], 2, "call_printf");
+}
+
+pub fn printInt(module: types.LLVMModuleRef, builder: types.LLVMBuilderRef, value: types.LLVMValueRef) !void {
+    const i32_ty = core.LLVMInt32Type();
+    var param_types = [_]types.LLVMTypeRef{ core.LLVMPointerType(core.LLVMInt8Type(), 0), i32_ty };
+    const printf_ty = core.LLVMFunctionType(i32_ty, param_types[0..], 2, 1);
+    if (printf_func == null) printf_func = core.LLVMAddFunction(module, "printf", printf_ty);
+
+    const fmt_str = core.LLVMBuildGlobalStringPtr(builder, "%d", "fmt");
+    var args = [_]types.LLVMValueRef{ fmt_str, value };
+    _ = core.LLVMBuildCall2(builder, printf_ty, printf_func, args[0..], 2, "call_printf");
+}
+
+pub fn printFloat(module: types.LLVMModuleRef, builder: types.LLVMBuilderRef, value: types.LLVMValueRef) !void {
+    const i32_ty = core.LLVMInt32Type();
+    const double_ty = core.LLVMDoubleType();
+    const param_types = [_]types.LLVMTypeRef{ core.LLVMPointerType(core.LLVMInt8Type(), 0), double_ty };
+    const printf_ty = core.LLVMFunctionType(i32_ty, param_types[0..], 2, 1);
+    if (printf_func == null) printf_func = core.LLVMAddFunction(module, "printf", printf_ty);
+
+    const fmt_str = core.LLVMBuildGlobalStringPtr(builder, "%f", "fmt");
+    var args = [_]types.LLVMValueRef{ fmt_str, value };
     _ = core.LLVMBuildCall2(builder, printf_ty, printf_func, args[0..], 2, "call_printf");
 }
